a18e0cad591f6c75e6093b6476cd374c7b64f084
k.darktiger@gmail.com <k.darktiger@gmail.com@4bc0f92c-dd86-c662-f982-7643e2c7abec>
2011-07-16 12:01:39 +0000

    - implemented FPSCounter

--- trunk/src/com/beatsportable/tsukikage/core/tools/FPSCounter.java
+++ trunk/src/com/beatsportable/tsukikage/core/tools/FPSCounter.java
@@ -1,5 +1,104 @@
 package com.beatsportable.tsukikage.core.tools;
 
-public class FPSCounter {
+import android.os.SystemClock;
 
+/**
+ * This is for calculating FPS values
+ */
+public abstract class FPSCounter {
+
+	// Private variables
+	private static int updateFrequency = 60;
+	private static long startTime, totalTime, pauseTime;
+	private static float frameCount, frameCountTotal;
+	private static float currentFPS, averageFPS;
+	private static String displayedFPS = "";
+	private static String formatFPS = "FPS %.2f/%.2f";
+	
+	/**
+	 * Number of frames per calculation update (default 60)
+	 */
+	public static void setUpdateFrequency(int frequency) {
+		updateFrequency = frequency;
+	}
+	
+	/**
+	 * Set formatting for displayed FPS string
+	 */
+	public static void setFormatting(String format) {
+		formatFPS = format;
+	}
+	
+	/**
+	 * Reset all values
+	 */
+	public static void restart() {
+		startTime = SystemClock.elapsedRealtime();
+		totalTime = 0;
+		pauseTime = 0;
+		frameCount = 0;
+		frameCountTotal = 0;
+		displayedFPS = "";
+	}
+	
+	/**
+	 * Call this on every frame
+	 */
+	public static void nextFrame() {
+		frameCount++;
+		if (frameCount >= updateFrequency) { // It should never go above updateFrequency, but just in case...
+			// Elapsed time since last update
+			long currentTime = SystemClock.elapsedRealtime();
+			long timeDiff = currentTime - startTime;
+			// Calculate current FPS
+			currentFPS = frameCount / ((float)timeDiff / 1000f);
+			// Update totals
+			frameCountTotal += frameCount;
+			totalTime += timeDiff;
+			averageFPS = frameCountTotal / ((float)totalTime / 1000f);
+			// Calculate average FPS
+			averageFPS = 
+			// Clear current data
+			frameCount = 0;
+			startTime = currentTime;
+			// Format displayed FPS string
+			displayedFPS = String.format(formatFPS, currentFPS, averageFPS);
+		}
+	}
+	
+	/**
+	 * Call this before pausing the game
+	 */
+	public static void pause() {
+		pauseTime = SystemClock.elapsedRealtime();
+	}
+	
+	/**
+	 * Call this after resuming the game
+	 */
+	public static void resume() {
+		startTime += SystemClock.elapsedRealtime() - pauseTime;
+		pauseTime = 0;
+	}
+	
+	/**
+	 * Get current FPS value
+	 */
+	public static float getCurrentFPS() {
+		return currentFPS;
+	}
+	
+	/**
+	 * Get average FPS value
+	 */
+	public static float getAverageFPS() {
+		return averageFPS;
+	}
+	
+	/**
+	 * Get formatted displayed FPS string
+	 */
+	public static String getDisplayedFPS() {
+		return displayedFPS;
+	}
 }
